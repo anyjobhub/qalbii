9d166a6e28adafec8031cdd1a7dde225a31b327a
Mohammad Khasim <mohammadkhasim.p@gmail.com>
2026-01-21 04:35:50 +0530

    fix: revert vite config - remove babel plugin causing build error
    Fast Refresh already works by default with @vitejs/plugin-react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Enable Fast Refresh
-      fastRefresh: true,
-      // Babel options for Fast Refresh
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
